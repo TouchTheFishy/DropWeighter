94242aa353fe646854f1b22be72bd92e294e4c9b
Paolo <paolodekeyzer@gmail.com>
2018-09-25 16:55:00 +0200

    trying to writezaefzf

--- DropWeighter.py
+++ DropWeighter.py
@@ -12,11 +12,13 @@ print ("started")
 time.sleep(1)
 try:
     print('Data Echo Mode Enabled')
+    i=0;
     while True:
-        print("reading")
+        print("reading",i)
         if ser.inWaiting() > 0:
             data = ser.read()
             print "Data: ",data
+            i+=1
             time.sleep(1)
 
 except KeyboardInterrupt:
