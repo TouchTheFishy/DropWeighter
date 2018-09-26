1f05112382f965ba9ed8435be498823ef0278680
Paolo <paolodekeyzer@gmail.com>
2018-09-26 15:30:02 +0200

    bugbash

--- DropWeighter.py
+++ DropWeighter.py
@@ -5,14 +5,14 @@ import xlwt as xw
 GPIO.setmode(GPIO.BOARD)
 print ("Starting program")
 
-ArduinoPin = 22
+ArduinoPin = 40
 
-now=time.strftime("%d%b_%H%M",time.localtime())
+now=time.strftime("%H%M_%d%b",time.localtime())
 fileName=now+".xls"
 book=xw.Workbook()
 sheet=book.add_sheet(str(now))
-sheet.write(0,0,'Iteration')
-sheet.write(0,1,'Weight')
+sheet.write(0,0,'Drop')
+sheet.write(0,1,'Weight (g)')
 GPIO.setup(ArduinoPin,GPIO.OUT)
 ser = serial.Serial('/dev/ttyS0', baudrate=9600,
                     parity=serial.PARITY_NONE,
@@ -21,16 +21,16 @@ ser = serial.Serial('/dev/ttyS0', baudrate=9600,
                     )
 print ("started")
 lastDrop=0
-lastweight=0
+lastWeight=0
 i=1
 while True:
     print "Dropping"
     lastDrop=int(round(time.time()*1000))
-    GPIO.output(22,1)
+    GPIO.output(ArduinoPin,1)
     while True:
         now = int(round(time.time() * 1000))
         if (now-lastDrop)>=50:
-            GPIO.output(22,0)
+            GPIO.output(ArduinoPin,0)
             break
 
     print "Waiting for stabilization"
@@ -50,8 +50,8 @@ while True:
                 data = ser.readline(16)
             parsed=data.split(" ")
             sheet.write(i, 0, i)
-            dropWeight=float(parsed[5])-lastweight
-            lastweight=float(parsed[5])
+            dropWeight=float(parsed[5])-lastWeight
+            lastWeight=float(parsed[5])
             sheet.write(i, 1, dropWeight)
             book.save(fileName)
             i += 1
--- test.py
+++ test.py
@@ -1,18 +1,5 @@
 import time
 import xlwt as xw
 
-now=time.strftime("%d%b_%H%M",time.localtime())
-fileName=now+".xls"
-book=xw.Workbook()
-sheet=book.add_sheet(str(now))
-sheet.write(0,0,'Iteration')
-sheet.write(0,1,'Weight')
-
-i=1
-while True:
-    data="lol"
-    sheet.write(i,0,i)
-    sheet.write(i,1,data)
-    book.save(fileName)
-    i+=1
-    time.sleep(1)
+now=time.strftime("%H%M_%d%b",time.localtime())
+print (now)
